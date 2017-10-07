ffc8526c6f6738a06d680b1c72450beac7ee5dd5
mastercljohnson <mastercljohnson@gmail.com>
2017-10-07 07:43:44 -0700

    Add Video

--- pods-native/App.js
+++ pods-native/App.js
@@ -1,52 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { ApolloProvider, graphql } from 'react-apollo';
-import { ApolloClient, createNetworkInterface } from 'apollo-client';
-
-import PodList from './components/Pods/PodList';
-import AddPod from './components/Pods/AddPod';
-
-import { GET_PODS } from './queries';
+import { View } from 'react-native';
+import Video from 'react-native-video';
 
 class App extends React.Component {
   render() {
-    const networkInterface = createNetworkInterface({
-      uri: 'http://localhost:3000/graphql',
-    });
-    const client = new ApolloClient({ networkInterface });
-
+    const URL = 'https://soundcloud.com/darude/sandstorm-radio-edit';
     return (
-      <ApolloProvider client={client}>
-        <Home />
-      </ApolloProvider>
-    );
-  }
-}
-
-@graphql(GET_PODS)
-class Home extends React.Component {
-  render() {
-    const { data } = this.props;
-    if (data.loading) return <Text>Loading</Text>
-    if (data.error) return <Text>{this.props.data.error.message}</Text> 
-
-    return (
-      <View style={styles.container}>
-        <PodList pods={this.props.data.pods} />
-        <AddPod />
+    <View>
+      <Video source={{uri: URL}}   // Can be a URL or a local file.
+         ref={(ref) => {
+           this.player = ref
+         }}                                      // Store reference
+         rate={1.0}                              // 0 is paused, 1 is normal.
+         volume={1.0}                            // 0 is muted, 1 is normal.
+         muted={false}                           // Mutes the audio entirely.
+         paused={false}                          // Pauses playback entirely.
+         resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+         repeat={false}                           // Repeat forever.
+         playInBackground={true}                // Audio continues to play when app entering background.
+         playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+         ignoreSilentSwitch={"obey"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+         progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+         onLoadStart={this.loadStart}            // Callback when video starts to load
+         onLoad={this.setDuration}               // Callback when video loads
+         onProgress={this.setTime}               // Callback every ~250ms with currentTime
+         onEnd={this.onEnd}                      // Callback when playback finishes
+         onError={this.videoError}               // Callback when video cannot be loaded
+         onBuffer={this.onBuffer}                // Callback when remote video is buffering
+         onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
+         />
       </View>
-    );
+  );
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
