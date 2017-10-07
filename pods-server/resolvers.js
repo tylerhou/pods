755be6702613a81243be2c02d872dc1892b3fc59
Tyler Hou <tyler.hou.cs@gmail.com>
2017-10-07 06:14:50 -0700

    Add get pod by id endpoint.

--- pods-server/resolvers.js
+++ pods-server/resolvers.js
@@ -6,11 +6,16 @@ const pods = [{
 let nextPodId = 1;
 let nextSongId = 0;
 
+const getPodById = id => pods.find(pod => pod.id == id);
+
 export const resolvers = {
   Query: {
     pods: () => {
       return pods;
     },
+    pod: (root, args) => {
+      return getPodById(args.id);
+    }
   },
   Mutation: {
     addPod: (root, args) => {
@@ -20,8 +25,7 @@ export const resolvers = {
     },
     addSong: (root, args) => {
       const song = { id: nextSongId++, track_url: args.track_url };
-      const pod = pods.find(pod => pod.id == args.pod_id);
-      console.log(pods, args);
+      const pod = getPodById(args.pod_id)
       pod.songs.push(song);
       return song;
     },
--- pods-server/schema.js
+++ pods-server/schema.js
@@ -18,6 +18,7 @@ const typeDefs = `
 
   type Query {
     pods: [Pod]
+    pod(id: ID!): Pod
   }
 
   type Mutation {
